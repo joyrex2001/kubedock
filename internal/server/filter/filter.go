cccb371ad403f00ad9d464862e3d5fa56452c0f4
Vincent van Dam <vincentd@erg.verweg.com>
2021-06-01 22:03:12 +0200

    Fix lint warnings

--- internal/server/filter/filter.go
+++ internal/server/filter/filter.go
@@ -5,20 +5,20 @@ import (
 	"strings"
 )
 
-// FilterRequest is the json filter argument.
+// Request is the json filter argument.
 // Unfortunately, depending on which docker-compose or "docker compose" you
 // run, the request may actually differ :-/ As "docker compose" doesn't
 // really rely on the server-side filtering (it will filter at the client
 // side as well), only the below type of request is supported for now...
-type FilterRequest map[string][]string
+type Request map[string][]string
 
-// FilterMatcher is the interface for a Match method to test the filter.
-type FilterMatcher interface {
+// Matcher is the interface for a Match method to test the filter.
+type Matcher interface {
 	Match(string, string, string) bool
 }
 
-// instance is the instace of this filter object
-type instance struct {
+// Filter is the instace of this filter object
+type Filter struct {
 	filters map[string][]keyval
 }
 
@@ -29,12 +29,12 @@ type keyval struct {
 }
 
 // New will return a new filter instance
-func New(f string) (*instance, error) {
-	in := &instance{
+func New(f string) (*Filter, error) {
+	in := &Filter{
 		filters: map[string][]keyval{},
 	}
 
-	rq := FilterRequest{}
+	rq := Request{}
 	if f != "" {
 		if err := json.Unmarshal([]byte(f), &rq); err != nil {
 			return in, err
@@ -59,7 +59,7 @@ func New(f string) (*instance, error) {
 
 // Match will call the matcher function and test if the object matches the
 // given key values.
-func (in *instance) Match(matcher FilterMatcher) bool {
+func (in *Filter) Match(matcher Matcher) bool {
 	res := true
 	for typ, filtrs := range in.filters {
 		for _, f := range filtrs {
