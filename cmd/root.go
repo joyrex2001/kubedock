811918c7ff0d33a5f8272b7866be62c5894a4f24
Vincent van Dam <vincentd@erg.verweg.com>
2021-06-03 21:32:53 +0200

    Clean up cli

--- cmd/root.go
+++ cmd/root.go
@@ -10,26 +10,6 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "kubedock",
 	Short: "Kubedock is a docker api to orchestrate containers on kubernetes.",
-	Long: `Kubedock is a docker api to orchestrate containers on kubernetes.
-
-  Kubedock is an minimal implementation of the docker api that will orchestrate
-  containers on a kubernetes cluster, rather than running containers locally.
-  Each container is considered to be short-lived and emphemeral, with the
-  intention they are solely used for running CI tests that require docker
-  containers inside a container. This enables running the tests without the
-  requirement of running docker-in-docker within resource heavy containers.
-
-Examples:
-  # run kubedock with defaults
-  kubedock server
-
-  # run kubedock in the current namespace, clean old resource, lock the
-  # namespace and use image inspect for retrieving ports of the image.
-  kubedock server -P -i --lock
-
-  # run kubedock in a namespace called kubedock and expose the api as a socket.
-  kubedock server --unix-socket /var/run/docker.sock -n kubedock
-`,
 }
 
 func Execute() {
--- cmd/server.go
+++ cmd/server.go
@@ -19,7 +19,7 @@ var cfgFile string
 
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "Run the kubedock api server",
+	Short: "Start the kubedock api server",
 	Run: func(cmd *cobra.Command, args []string) {
 		flag.Set("v", viper.GetString("verbosity"))
 		internal.Main()
@@ -32,38 +32,35 @@ func init() {
 	klog.InitFlags(nil)
 	// pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 
-	// for now; let's just add everything to rootCmd, as there is only
-	// one 'real' cmd (this 'server') it is probably more intuitive to
-	// the user to see all available args immediately.
-	rootCmd.PersistentFlags().String("listen-addr", ":8999", "Webserver listen address")
-	rootCmd.PersistentFlags().String("unix-socket", "", "Unix socket to listen to (instead of port)")
-	rootCmd.PersistentFlags().Bool("tls-enable", false, "Enable TLS on api server")
-	rootCmd.PersistentFlags().String("tls-key-file", "", "TLS keyfile")
-	rootCmd.PersistentFlags().String("tls-cert-file", "", "TLS certificate file")
-	rootCmd.PersistentFlags().StringP("namespace", "n", getContextNamespace(), "Namespace in which containers should be orchestrated")
-	rootCmd.PersistentFlags().String("initimage", config.Image, "Image to use as initcontainer for volume setup")
-	rootCmd.PersistentFlags().BoolP("inspector", "i", false, "Enable image inspect to fetch container port config from a registry")
-	rootCmd.PersistentFlags().DurationP("timeout", "t", 1*time.Minute, "Container creating timeout")
-	rootCmd.PersistentFlags().DurationP("reapmax", "r", 15*time.Minute, "Reap all resources older than this time")
-	rootCmd.PersistentFlags().Bool("lock", false, "Lock namespace for this instance")
-	rootCmd.PersistentFlags().Duration("lock-timeout", 15*time.Minute, "Max time trying to acquire namespace lock")
-	rootCmd.PersistentFlags().StringP("verbosity", "v", "1", "Log verbosity level")
-	rootCmd.PersistentFlags().BoolP("prune-start", "P", false, "Prune all existing kubedock resources before starting")
+	serverCmd.PersistentFlags().String("listen-addr", ":8999", "Webserver listen address")
+	serverCmd.PersistentFlags().String("unix-socket", "", "Unix socket to listen to (instead of port)")
+	serverCmd.PersistentFlags().Bool("tls-enable", false, "Enable TLS on api server")
+	serverCmd.PersistentFlags().String("tls-key-file", "", "TLS keyfile")
+	serverCmd.PersistentFlags().String("tls-cert-file", "", "TLS certificate file")
+	serverCmd.PersistentFlags().StringP("namespace", "n", getContextNamespace(), "Namespace in which containers should be orchestrated")
+	serverCmd.PersistentFlags().String("initimage", config.Image, "Image to use as initcontainer for volume setup")
+	serverCmd.PersistentFlags().BoolP("inspector", "i", false, "Enable image inspect to fetch container port config from a registry")
+	serverCmd.PersistentFlags().DurationP("timeout", "t", 1*time.Minute, "Container creating timeout")
+	serverCmd.PersistentFlags().DurationP("reapmax", "r", 15*time.Minute, "Reap all resources older than this time")
+	serverCmd.PersistentFlags().Bool("lock", false, "Lock namespace for this instance")
+	serverCmd.PersistentFlags().Duration("lock-timeout", 15*time.Minute, "Max time trying to acquire namespace lock")
+	serverCmd.PersistentFlags().StringP("verbosity", "v", "1", "Log verbosity level")
+	serverCmd.PersistentFlags().BoolP("prune-start", "P", false, "Prune all existing kubedock resources before starting")
 
-	viper.BindPFlag("server.listen-addr", rootCmd.PersistentFlags().Lookup("listen-addr"))
-	viper.BindPFlag("server.socket", rootCmd.PersistentFlags().Lookup("unix-socket"))
-	viper.BindPFlag("server.tls-enable", rootCmd.PersistentFlags().Lookup("tls-enable"))
-	viper.BindPFlag("server.tls-cert-file", rootCmd.PersistentFlags().Lookup("tls-cert-file"))
-	viper.BindPFlag("server.tls-key-file", rootCmd.PersistentFlags().Lookup("tls-key-file"))
-	viper.BindPFlag("kubernetes.namespace", rootCmd.PersistentFlags().Lookup("namespace"))
-	viper.BindPFlag("kubernetes.initimage", rootCmd.PersistentFlags().Lookup("initimage"))
-	viper.BindPFlag("kubernetes.timeout", rootCmd.PersistentFlags().Lookup("timeout"))
-	viper.BindPFlag("registry.inspector", rootCmd.PersistentFlags().Lookup("inspector"))
-	viper.BindPFlag("reaper.reapmax", rootCmd.PersistentFlags().Lookup("reapmax"))
-	viper.BindPFlag("lock.enabled", rootCmd.PersistentFlags().Lookup("lock"))
-	viper.BindPFlag("lock.timeout", rootCmd.PersistentFlags().Lookup("lock-timeout"))
-	viper.BindPFlag("verbosity", rootCmd.PersistentFlags().Lookup("verbosity"))
-	viper.BindPFlag("prune-start", rootCmd.PersistentFlags().Lookup("prune-start"))
+	viper.BindPFlag("server.listen-addr", serverCmd.PersistentFlags().Lookup("listen-addr"))
+	viper.BindPFlag("server.socket", serverCmd.PersistentFlags().Lookup("unix-socket"))
+	viper.BindPFlag("server.tls-enable", serverCmd.PersistentFlags().Lookup("tls-enable"))
+	viper.BindPFlag("server.tls-cert-file", serverCmd.PersistentFlags().Lookup("tls-cert-file"))
+	viper.BindPFlag("server.tls-key-file", serverCmd.PersistentFlags().Lookup("tls-key-file"))
+	viper.BindPFlag("kubernetes.namespace", serverCmd.PersistentFlags().Lookup("namespace"))
+	viper.BindPFlag("kubernetes.initimage", serverCmd.PersistentFlags().Lookup("initimage"))
+	viper.BindPFlag("kubernetes.timeout", serverCmd.PersistentFlags().Lookup("timeout"))
+	viper.BindPFlag("registry.inspector", serverCmd.PersistentFlags().Lookup("inspector"))
+	viper.BindPFlag("reaper.reapmax", serverCmd.PersistentFlags().Lookup("reapmax"))
+	viper.BindPFlag("lock.enabled", serverCmd.PersistentFlags().Lookup("lock"))
+	viper.BindPFlag("lock.timeout", serverCmd.PersistentFlags().Lookup("lock-timeout"))
+	viper.BindPFlag("verbosity", serverCmd.PersistentFlags().Lookup("verbosity"))
+	viper.BindPFlag("prune-start", serverCmd.PersistentFlags().Lookup("prune-start"))
 
 	viper.BindEnv("server.listen-addr", "SERVER_LISTEN_ADDR")
 	viper.BindEnv("server.tls-enable", "SERVER_TLS_ENABLE")
@@ -76,11 +73,11 @@ func init() {
 
 	// kubeconfig
 	if home := homeDir(); home != "" {
-		rootCmd.PersistentFlags().String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		serverCmd.PersistentFlags().String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
-		rootCmd.PersistentFlags().String("kubeconfig", "", "absolute path to the kubeconfig file")
+		serverCmd.PersistentFlags().String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
-	viper.BindPFlag("kubernetes.kubeconfig", rootCmd.PersistentFlags().Lookup("kubeconfig"))
+	viper.BindPFlag("kubernetes.kubeconfig", serverCmd.PersistentFlags().Lookup("kubeconfig"))
 }
 
 // homeDir returns the current home directory of the user.
